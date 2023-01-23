8be50edc32684e2d647b81ca551192d3083dbbe9
von-demo <von-demo@Bryans-MBP.attlocal.net>
2023-01-22 19:29:13 -0500

    add

--- main.py
+++ main.py
@@ -49,41 +49,44 @@ if __name__ == "__main__":
          print('my_api_parent_id is: ' + parent_id)
 
          # create resource
-         cmd_a = f'aws apigateway create-resource --rest-api-id {api_id} --parent-id {parent_id} --path-part {{proxy+}}'
-         print(cmd_a)
-         out_a = run_bash(cmd_a)
-         resource_id = yaml.safe_load(out_a)['id']
-         print(out_a)
+         cmd_4 = f'aws apigateway create-resource --rest-api-id {api_id} --parent-id {parent_id} --path-part {{proxy+}}'
+         out_4 = run_bash(cmd_4)
+         resource_id = yaml.safe_load(out_4)['id']
+         print(out_4)
 
          # apply put-method to api
-         cmd_4 = f'aws apigateway put-method --rest-api-id {api_id} --resource-id {resource_id} --http-method "ANY" --authorization-type "NONE"'
-         print(cmd_4)
-         out_4 = run_bash(cmd_4)
-         print(out_4)
+         cmd_5 = f'aws apigateway put-method --rest-api-id {api_id} --resource-id {resource_id} --http-method "ANY" --authorization-type "NONE"'
+         out_5 = run_bash(cmd_5)
+         print(out_5)
          
          # apply put-integration to api, note: look into differences between AWS and AWS_PROXY. response body seems to change slightly.
          cmd_6 = f'aws apigateway put-integration --rest-api-id {api_id} --resource-id {resource_id} --http-method ANY --type AWS_PROXY --integration-http-method POST --uri "arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/{lambda_url}/invocations"'
-         print(cmd_6)
          out_6 = run_bash(cmd_6)
          print(out_6)
 
          # give lambda permissions to be run by your api.
-         cmd_8 = f'aws lambda add-permission --function-name {function_aws} --statement-id "{api_id}" --action lambda:InvokeFunction --principal apigateway.amazonaws.com --source-arn "arn:aws:execute-api:us-east-1:482102633168:{api_id}/*"'
-         print(cmd_8)
+         cmd_7 = f'aws lambda add-permission --function-name {function_aws} --statement-id "{api_id}" --action lambda:InvokeFunction --principal apigateway.amazonaws.com --source-arn "arn:aws:execute-api:us-east-1:482102633168:{api_id}/*"'
+         out_7 = run_bash(cmd_7)
+         print(out_7)
+         
+         # deploy api to connect lambda with api gateway
+         cmd_8 = f'aws apigateway create-deployment --rest-api-id {api_id} --stage-name "dev"'
          out_8 = run_bash(cmd_8)
          print(out_8)
          
-         # deploy api to connect lambda with api gateway
-         cmd_9 = f'aws apigateway create-deployment --rest-api-id {api_id} --stage-name "dev"'
-         print(cmd_9)
-         out_9 = run_bash(cmd_9)
-         print(out_9)
-         
 
 # TO DO
-# 1. figure out how to modify the payload being submitted via api gateway (for parametrized lambdas). I believe the payload has to be very explicitly defined
-# 2. modify the lambda_routes yamls as needed to accept some common format that defines the payload
-# 3. dynamically modify each statement here to deploy lambdas as needed with their respective payload
-# 4. figure out how to block all api-gateway APIs from the public internet
-# 5. move to vontive infra
-# 6. only allow access via VPC peering
+# 1. figure out how to modify the payload being submitted via api gateway (for parametrized lambdas). I believe the payload has to be very explicitly defined; COMPLETE
+# 2. modify the lambda_routes yamls as needed to accept some common format that defines the payload; COMPLETE, via aws_proxy
+# 3. dynamically modify each statement here to deploy lambdas as needed with their respective payload ; COMPLETE, cia aws_proxy
+# 4. figure out how to block all api-gateway APIs from the public internet TO DO
+# 5. move to vontive infra TO DO
+# 6. only allow access via VPC peering TO DO
+
+
+# next steps to test 4-6:
+# sample: curl https://4npjdwmgh7.execute-api.us-east-1.amazonaws.com/dev/placeholder\?name_2\=bryan
+# make an ec2 instance in us-west-1. from here, run the curl command to test.
+# IF everything is successfuly, then curl from EC2 works, but curl from your local laptop does NOT work.
+# vpc peering may be the answer here, not sure.
+# also clean up existing AWS infra
